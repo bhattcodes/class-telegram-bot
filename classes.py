ada0427e1d79376ddc62c58af95dcc282d7e5bed
Aman Bhatt <bhatt.codes@gmail.com>
2021-09-07 12:31:43 +0530

    updated

--- classes.py
+++ classes.py
@@ -2,7 +2,18 @@ import time
 import requests
 # Monday
 classes_0 = [   
-    {"time_hour" : "9", "time_min": "0" , "message" : "Class: ,\n Meet Link: https://meet.google.com/sar-awhh-ydy ,\n GCR link:"},
+    {"time_hour" : "9", "time_min": "0" , "message" : "Class: ,\n Meet Link: https://meet.google.com/sar-awhh-ydy , GCR link:"},
+    {"time_hour" : "9", "time_min": "0" , "message" : "Class: ,\n Meet Link: https://meet.google.com/sar-awhh-ydy , GCR link:"},
+    ]
+# Tuesday
+classes_1 = [   
+    {"time_hour" : "9", "time_min": "0" , "message" : "Class: ,\n Meet Link: https://meet.google.com/sar-awhh-ydy , GCR link:"},
+    {"time_hour" : "9", "time_min": "0" , "message" : "Class: ,\n Meet Link: https://meet.google.com/sar-awhh-ydy , GCR link:"},
+    ]
+# Wednesday
+classes_2 = [   
+    {"time_hour" : "9", "time_min": "0" , "message" : "Class: ,\n Meet Link: https://meet.google.com/sar-awhh-ydy , GCR link:"},
+    {"time_hour" : "9", "time_min": "0" , "message" : "Class: ,\n Meet Link: https://meet.google.com/sar-awhh-ydy , GCR link:"},
     ]
 
 print(classes_0)
